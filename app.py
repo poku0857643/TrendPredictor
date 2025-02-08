86d035cbe052e79c01ba45db371cbe2ec64ab9af
lucy <lucy199043@gmail.com>
2025-02-08 22:11:53 +0800

    add title to the page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from prophet import Prophet
 
+st.title("Stock Price Trend")
+
 # Load data
 data = pd.read_csv('data.csv')
 
@@ -11,14 +13,12 @@ data['date'] = pd.to_datetime(data['date'], errors='coerce')
 # If 'ds' contains strings or mixed types, it will convert invalid dates to NaT
 # Drop any rows with invalid 'ds' values (NaT values)
 data = data.dropna(subset=['date'])
-#data['close'] = pd.to_numeric(data['close'], errors='coerce')  # Converts invalid entries to NaN
 
 # Convert 'y' to numeric, coercing errors to NaN
 data['change'] = pd.to_numeric(data['change'], errors='coerce')
 
 # Drop rows with NaN values in 'y' (in case there were invalid values)
 data = data.dropna(subset=['change'])
-
 
 # Prepare the data for Prophet
 data['date'] = pd.to_datetime(data['date'])
